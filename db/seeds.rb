20e0a2f511ac672a10be89e01d4d7ad277e96866
Andrew Day <day_andy@hotmail.com>
2017-10-10 02:22:30 +0000

    created seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "My blog post #{blog}",
+        body: "Body"
+        )
+end
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15)
+    end
+    
+9.times do |portfolio_item|
+   Portfolio.create!(
+       title: "Portfolio title: #{portfolio_item}",
+       subtitle: "My great service",
+       body: "Body",
+       main_image: "http://placehold.it/600x400" ,
+       thumb_image:  "http://placehold.it/300x200" 
+    )         
+end
