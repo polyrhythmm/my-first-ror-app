bde235a2d15ae6fd45f387c7670bc9b691e00dff
Andrew Day <day_andy@hotmail.com>
2017-10-11 01:31:35 +0000

    added blogs controller functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
    
@@ -8,7 +10,13 @@ Rails.application.routes.draw do
   
   root to: 'pages#home'
 
-  resources :blogs
+  resources :blogs do 
+    member do 
+      get :toggle_status
+    end
+  end
+  
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
